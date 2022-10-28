bef53aa86de7e3aa524ee59b42b59709ca0183a9
NatalkaMB <org.com1724@gmail.com>
2022-10-28 23:59:05 +0300

    Add file product filter testing- TestRail.pdf

--- SQL_JOIN.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM cities join hotels on cities.cityID = hotels.hotelID;
-
-SELECT * FROM cities INNER join hotels on cities.cityID = hotels.hotelID;
-
-SELECT * FROM cities left join hotels on cities.cityID = hotels.hotelID;
-
-SELECT * FROM cities right join hotels on cities.cityID = hotels.hotelID
-union all
-SELECT * FROM cities left join hotels on cities.cityID = hotels.hotelID where cities.cityID is null;
